7776ccf670d6c3ac0266e37a9384ac19525e5c87
Raki <824691+Raki@users.noreply.github.com>
2023-02-10 00:49:40 -0700

    Update VKBackend.cpp
    Little cleanup

--- utils/VKBackend.cpp
+++ utils/VKBackend.cpp
@@ -378,7 +378,6 @@ namespace VKBackend
 		VkSubpassDependency dependency{};
 		dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
 		dependency.dstSubpass = 0;
-		dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
 		dependency.srcAccessMask = 0;
 		//dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
 		//dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
