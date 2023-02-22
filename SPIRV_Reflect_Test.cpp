dbf199c8406a66fe05dcc2eee84cd5a062afc47d
Raki <824691+Raki@users.noreply.github.com>
2023-02-21 19:56:45 -0700

    Added support for SPIRV-Reflect

---
+++ SPIRV_Reflect_Test.cpp
@@ -0,0 +1,47 @@
+
+#include "CommonHeaders.h"
+#include "Utility.h"
+#include "VKUtility.h"
+#include "VKBackend.h"
+#include "spirv_reflect.h"
+
+
+/*
+* Goal of this program is to explore SPIRV-Reflect
+* https://github.com/KhronosGroup/SPIRV-Reflect
+*/
+int main()
+{
+    auto data = Utility::readBinaryFileContents("shaders/blend2.vert.spv");
+
+    if (data.size() == 0)
+        return EXIT_FAILURE;
+
+    auto s = sizeof(unsigned char);
+
+    // Generate reflection data for a shader
+    SpvReflectShaderModule module;
+    SpvReflectResult result = spvReflectCreateShaderModule(data.size()*sizeof(unsigned char), data.data(), &module);
+    assert(result == SPV_REFLECT_RESULT_SUCCESS);
+
+    // Enumerate and extract shader's input variables
+    uint32_t var_count = 0;
+    result = spvReflectEnumerateInputVariables(&module, &var_count, NULL);
+    assert(result == SPV_REFLECT_RESULT_SUCCESS);
+    SpvReflectInterfaceVariable** input_vars =
+        (SpvReflectInterfaceVariable**)malloc(var_count * sizeof(SpvReflectInterfaceVariable*));
+    result = spvReflectEnumerateInputVariables(&module, &var_count, input_vars);
+    assert(result == SPV_REFLECT_RESULT_SUCCESS);
+
+
+    for (size_t i=0;i<var_count;i++)
+    {
+        auto ip = input_vars[i];
+        fmt::print("{}\n",ip->name);
+    }
+
+    // Destroy the reflection data when no longer required.
+    spvReflectDestroyShaderModule(&module);
+
+    return EXIT_SUCCESS;
+}
