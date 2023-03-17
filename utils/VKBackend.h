ba58673b23147a53cec3a0764da5aa62f36a8633
Raki <824691+Raki@users.noreply.github.com>
2023-03-16 20:52:24 -0600

    vertexinput attrib desc from spirv
    completed

--- utils/VKBackend.h
+++ utils/VKBackend.h
@@ -98,6 +98,7 @@ namespace VKBackend
 
 	std::vector<DescriptorSetLayoutData> getDescriptorSetLayoutDataFromSpv(const std::string path);
 	std::vector<DescriptorSetLayoutData> getDescriptorSetLayoutDataFromSpv(const std::vector<unsigned char> &fileContent);
+	uint32_t formatSize(VkFormat format);
 	void getInputInfoFromSpv(const std::vector<unsigned char>& fileContent, std::vector<VkVertexInputAttributeDescription> &vertIPAttribDesc,
 		VkVertexInputBindingDescription &vertIPBindDesc);
 	void createDescriptorSetLayout(std::vector <VkDescriptorSetLayoutBinding> layoutBindings);
